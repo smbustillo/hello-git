5e8288cad83767522fb8b4325c43f89b33172b24
smartbus <smbustill@gmail.com>
2025-02-05 16:50:06 +0100

    git3 v.login

--- HelloGit3.java
+++ HelloGit3.java
@@ -1,6 +1,6 @@
 public class HelloGit3 {
     public static void main(String[] args) {
-        System.out.println("Hello, Git3 v.2!"); 
+        System.out.println("Hello, Git3 v.login!"); 
     }
     
 }
